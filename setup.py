6c887ac26ec3013c1833cea49b33e1f5702b0121
siegmone <simone.vortice@proton.me>
2024-08-03 20:49:31 +0200

    Fixed workflow and setup.py

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ from setuptools import find_packages, setup, Command
 NAME = 'eis'
 DESCRIPTION = 'My short description for my project.'
 URL = 'https://github.com/siegmone/python-eis'
-# EMAIL = 'me@example.com'
+EMAIL = 'simone.vortice@proton.me'
 AUTHOR = 'siegmone'
-REQUIRES_PYTHON = '>=3.6.0'
+REQUIRES_PYTHON = '>=3.10.0'
 VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
@@ -93,39 +93,39 @@ class UploadCommand(Command):
 
 
 # Where the magic happens:
-setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
+# setup(
+#     name=NAME,
+#     version=about['__version__'],
+#     description=DESCRIPTION,
+#     long_description=long_description,
+#     long_description_content_type='text/markdown',
+#     author=AUTHOR,
+#     author_email=EMAIL,
+#     python_requires=REQUIRES_PYTHON,
+#     url=URL,
+#     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+#     # If your package is a single module, use this instead of 'packages':
+#     # py_modules=['mypackage'],
 
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    include_package_data=True,
-    license='MIT',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
-)
+#     # entry_points={
+#     #     'console_scripts': ['mycli=mymodule:cli'],
+#     # },
+#     install_requires=REQUIRED,
+#     extras_require=EXTRAS,
+#     include_package_data=True,
+#     license='MIT',
+#     classifiers=[
+#         # Trove classifiers
+#         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+#         'License :: OSI Approved :: MIT License',
+#         'Programming Language :: Python',
+#         'Programming Language :: Python :: 3',
+#         'Programming Language :: Python :: 3.10',
+#         'Programming Language :: Python :: Implementation :: CPython',
+#         'Programming Language :: Python :: Implementation :: PyPy'
+#     ],
+#     # $ setup.py publish support.
+#     cmdclass={
+#         'upload': UploadCommand,
+#     },
+# )
